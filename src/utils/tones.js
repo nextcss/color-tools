e6e9cd7e07a8169ea12ab8d8db33e2a347829403
Zsolt Tovis <tovis.zsolt@gmail.com>
2022-12-05 13:33:46 +0100

    cleaning

--- src/utils/tones.js
+++ src/utils/tones.js
@@ -1,16 +1,16 @@
-import { colorShift } from './shift.js';
+import { colorShift } from "./shift.js";
 
 const light = [-90, -85, -74, -62, -50, -40, -30, -20, -10];
-const dark = [10, 20, 30, 40, 50, 60, 70, 80, 85];
+const dark = [0, 10, 20, 30, 40, 50, 60, 70, 80, 85];
 
 export const toneMap = (hex) => {
   const results = {};
 
-  [...light, 0, ...dark].forEach((tone, key) => {
-    const toneKey = key * 50 + 50;
+  [...light, ...dark].forEach((tone, key) => {
+    const index = key * 50 + 50;
     const color = colorShift(hex, tone);
 
-    if (color) results[toneKey] = color;
+    if (color) results[index] = color;
   });
 
   return results;
