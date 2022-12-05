7a9ce56c65e51e08589abc4181a7b7b6b12f90ea
Zsolt Tovis <tovis.zsolt@gmail.com>
2022-12-05 13:22:06 +0100

    cleaning

--- src/utils/rgb2hex.js
+++ src/utils/rgb2hex.js
@@ -1,7 +1,4 @@
 export const rgb2hex = (rgb) => {
   if (!Array.isArray(rgb) || rgb.length !== 3) return undefined;
-
-  const result = rgb.map((val) => val.toString(16).padStart(2, '0')).join('');
-
-  return result ? '#' + result : undefined;
+  return '#' + rgb.map((val) => val.toString(16).padStart(2, '0')).join('');
 };
