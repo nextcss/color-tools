80137f4327c68e9da373f3b91f728437541beccd
Zsolt Tovis <tovis.zsolt@gmail.com>
2022-12-05 13:17:44 +0100

    fix secription / add empty array exception

--- test/rgb2hex.test.js
+++ test/rgb2hex.test.js
@@ -1,16 +1,20 @@
 import { rgb2hex } from '../src/index.js';
 
-describe('HEX to RGB', () => {
+describe('RGB to HEX', () => {
   test.concurrent('3 digits array', async () => {
     expect(rgb2hex([238, 238, 238])).toBe('#eeeeee');
   });
 
-  test.concurrent('2 digits array', async () => {
+  test.concurrent('Exception: 2 digits array', async () => {
     expect(rgb2hex([238, 238])).toBeUndefined();
   });
 
-  test.concurrent('1 digits array', async () => {
+  test.concurrent('Exception: 1 digits array', async () => {
     expect(rgb2hex([238])).toBeUndefined();
+  });
+
+  test.concurrent('Exception: empty array', async () => {
+    expect(rgb2hex([])).toBeUndefined();
   });
 
   test.concurrent('Exception: empty string', async () => {
