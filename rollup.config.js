cbd73553ae9f036b15a496b23252b7272a82efcf
Zsolt Tovis <tovis.zsolt@gmail.com>
2022-12-19 02:35:14 +0100

    fix output names

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
-import { terser } from 'rollup-plugin-terser';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import { terser } from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 const configTerser = { format: { comments: false } };
 const modulePlugins = [resolve(), commonjs(), terser(configTerser)];
 
 export default [
   {
-    input: 'src/index.js',
+    input: "src/index.js",
     output: [
-      { name: 'node-cjs', file: 'dist/index.js', format: 'es' },
-      { name: 'node-es', file: 'dist/index.cjs', format: 'cjs' },
+      { name: "node-es", file: "dist/index.js", format: "es" },
+      { name: "node-cjs", file: "dist/index.cjs", format: "cjs" },
     ],
     plugins: modulePlugins,
   },
