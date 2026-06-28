befad19f36a455def2a9f6d6d83bdadf4344e6da
Zsolt Tovis <tovis.zsolt@gmail.com>
2026-06-28 03:22:24 +0200

    fix re-exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+// Colors
+export * from './colors/hsl.js';
+export * from './colors/hwb.js';
+export * from './colors/oklab.js';
+export * from './colors/rgb.js';
+
+// Tools
 export * from './tools/brightness.js';
 export * from './tools/colorify.js';
-export * from './tools/hex-hsl.js';
-export * from './tools/hex-rgb.js';
 export * from './tools/random.js';
 export * from './tools/shift.js';
 export * from './tools/tones.js';
